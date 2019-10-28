f8cd169b09ec00c503d7a3374ece3905724c6827
mansoora <mumtmal@gmail.com>
2019-10-28 00:55:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function whisper(String){
   return String.toLowerCase()
 }
 function logShout(string){
+<<<<<<< HEAD
   console.log(string.toUpperCase())
   
 }
@@ -17,4 +18,10 @@ function sayHiToGrandma(string){
   if (string===string.toLowerCase()) return "I can't hear you!";
   else if (string=== string.toUpperCase()) return "YES INDEED!";
   else if (string=== "I love you, Grandma.") return "I love you, too.";
+=======
+  var uppercase = "HELLO!"
+uppercase.toUpperCase()
+  console.log(upperCase)
+  
+>>>>>>> 9bc3234f792d9f4fa18b78dd8a675c7e92388024
 }
